0fc42974fcdeb25d283b806690699673985bfafe
FranzJvezNiceAxa <56778463+stayhydated@users.noreply.github.com>
2025-08-20 15:57:08 -0400

    .

--- examples/bevy-example/src/main.rs
+++ examples/bevy-example/src/main.rs
@@ -63,6 +63,7 @@ struct ButtonText;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(AssetPlugin {
+            watch_for_changes_override: Some(true),
             file_path: "../assets".to_string(),
             ..default()
         }))
