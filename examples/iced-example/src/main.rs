591ba565410292f535cbc575602828ff532e39cd
FranzJvezNiceAxa <56778463+stayhydated@users.noreply.github.com>
2025-10-11 13:06:53 -0400

    ☒

--- examples/iced-example/src/main.rs
+++ examples/iced-example/src/main.rs
@@ -1,10 +1,18 @@
 use es_fluent::{EsFluent, ToFluentString};
 use example_shared_lib::{ButtonState, Languages};
+use iced::font::{Family, Font};
 use iced::widget::{button, center, mouse_area, row, text};
 use iced::{Center, Element, Theme};
 use strum::IntoEnumIterator;
 
 mod i18n;
+
+const NOTO_SANS_SC: &[u8] = include_bytes!("../../assets/fonts/NotoSansSC-Bold.ttf");
+const FONT: Font = Font {
+    family: Family::Name("Noto Sans SC"),
+    weight: iced::font::Weight::Bold, // normal doesnt render cn? ig this demo gon have ☒
+    ..Font::DEFAULT
+};
 
 #[derive(Clone, Copy, Debug, EsFluent)]
 pub enum IcedScreenMessages {
@@ -16,7 +24,8 @@ pub fn main() -> iced::Result {
     i18n::change_locale(&Languages::English.into()).unwrap();
 
     iced::application("", IcedExampleView::update, IcedExampleView::view)
-        .font(include_bytes!("../../assets/fonts/NotoSansSC-Bold.ttf").as_slice())
+        .font(NOTO_SANS_SC)
+        .default_font(FONT)
         .theme(IcedExampleView::theme)
         .run()
 }
@@ -68,7 +77,11 @@ impl IcedExampleView {
     }
 
     fn view(&self) -> Element<'_, Message> {
-        let button_widget = |label| button(text(label).align_x(Center)).padding(10).width(150);
+        let button_widget = |label| {
+            button(text(label).font(FONT).align_x(Center))
+                .padding(10)
+                .width(150)
+        };
 
         let state_button = mouse_area(button_widget(self.button_state.to_fluent_string()))
             .on_enter(Message::StateButtonHovered(true))
