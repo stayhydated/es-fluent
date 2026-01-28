6b158e8bd633eaf56a7dc1de8271df7e2c9a46ba
stayhydated <56778463+stayhydated@users.noreply.github.com>
2026-01-27 21:41:17 -0500

    sitemap

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,17 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
-	build: {
-		assets: "_assets",
+
+  build: {
+      assets: "_assets",
 	},
-	server: {
-		headers: {
-			"Cross-Origin-Embedder-Policy": "require-corp",
-			"Cross-Origin-Opener-Policy": "same-origin",
-		},
+
+  server: {
+      headers: {
+          "Cross-Origin-Embedder-Policy": "require-corp",
+          "Cross-Origin-Opener-Policy": "same-origin",
+      },
 	},
+
+  integrations: [sitemap()],
 });
