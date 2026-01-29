7e085b016f5ef977114ca780e00abc67728c15e4
stayhydated <56778463+stayhydated@users.noreply.github.com>
2026-01-28 19:39:19 -0500

    fix ts?

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	site: "https://stayhydated.github.io",
+	base: "/es-fluent",
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
